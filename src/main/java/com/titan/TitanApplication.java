2db60babff76484e9f49219df3d7a0516edd9281
yohagos <yosef.hagos@googlemail.com>
2023-12-15 14:48:00 +0100

    feat: app configs

---
+++ src/main/java/com/titan/TitanApplication.java
@@ -0,0 +1,13 @@
+package com.titan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TitanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TitanApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/titan/config/ApplicationConfig.java
@@ -0,0 +1,49 @@
+package com.titan.config;
+
+import com.titan.user.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+@RequiredArgsConstructor
+public class ApplicationConfig {
+
+    private final UserRepository userRepository;
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        return username -> userRepository
+                .findUserByEmail(username)
+                .orElseThrow(() -> new UsernameNotFoundException("user "+ username + " not found"));
+    }
+
+    @Bean
+    public AuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+        provider.setUserDetailsService(userDetailsService());
+        provider.setPasswordEncoder(passwordEncoder());
+        return provider;
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(
+            AuthenticationConfiguration config
+    ) throws  Exception {
+        return config.getAuthenticationManager();
+    }
+}
