ed7a561e3c6492bc871872bedf3e146284963c2f
yohagos <yosef.hagos@googlemail.com>
2024-01-20 17:01:08 +0100

    fix: first draft

--- src/test/java/com/titan/user/UserControllerTest.java
+++ src/test/java/com/titan/user/UserControllerTest.java
@@ -1,17 +1,27 @@
 package com.titan.user;
 
+import com.titan.config.SecurityConfiguration;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Import;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.*;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@ExtendWith(SpringExtension.class)
+@WebMvcTest(controllers = UserController.class)
+@Import(SecurityConfiguration.class)
 class UserControllerTest {
 
-    @Autowired
-    private MockMvc mockMvc;
+    @MockBean private UserService userService;
+
+    @Autowired private MockMvc mockMvc;
 
     @Test
     void getAllUsers() throws Exception {
--- src/test/java/com/titan/user/UserServiceTest.java
+++ src/test/java/com/titan/user/UserServiceTest.java
@@ -1,20 +1,71 @@
 package com.titan.user;
 
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
 
 class UserServiceTest {
 
-    @Test
-    void getUserList() {
+    static UserEntity user1 = new UserEntity(10L, "test", "test", "test10@test.de", "test", 1111, LocalDateTime.now(), UserRole.USER);
+    static UserEntity user2 = new UserEntity(20L, "test", "test", "test20@test.de", "test", 2222, LocalDateTime.now(), UserRole.USER);
+    static UserEntity user3 = new UserEntity(30L, "test", "test", "test30@test.de", "test", 3333, LocalDateTime.now(), UserRole.USER);
+    static UserEntity user4 = new UserEntity(40L, "test", "test", "test40@test.de", "test", 4444, LocalDateTime.now(), UserRole.USER);
+    static UserEntity user5 = new UserEntity(50L, "test", "test", "test50@test.de", "test", 5555, LocalDateTime.now(), UserRole.USER);
+
+    static List<UserEntity> userEntities = new ArrayList<>();
+
+    @InjectMocks
+    private UserService userService;
+    @Mock
+    private UserRepository userRepository;
+
+    @BeforeAll
+    public static void setUpAll() {
+        setUpUserList();
+    }
+
+    @BeforeEach
+    public void setUp() {
+        MockitoAnnotations.openMocks(this);
     }
 
     @Test
-    void updateUserPin() {
+    @DisplayName("getUserList()")
+    public void shouldReturnListOfFiveUsers() {
+        when(userService.getUserList())
+                .thenReturn(userEntities);
+        when(userRepository.findAll())
+                .thenReturn(userEntities);
+        var userList = userService.getUserList();
+        assertEquals(userEntities.size(), userList.size(), "Get user list error");
     }
 
     @Test
-    void checkUserPin() {
+    @DisplayName("checkUserPin")
+    public void shouldFindAUserDependingOnThePin() {
+        when(userService.checkUserPin(user1.getPin()))
+                .thenReturn(user1);
+        when(userRepository.findUserByPin(user1.getPin()))
+                .thenReturn(Optional.of(user1));
+        var user = userService.checkUserPin(1111);
+        assertEquals(user1, user, "optional user and test user are not equal");
+    }
+
+    private static void setUpUserList() {
+        userEntities = List.of(user1,user2,user3,user4,user5);
     }
 }
