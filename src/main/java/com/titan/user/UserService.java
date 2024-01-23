75d5a70cfaaaa2fd88e2b6fd9cf9aa0d43f04f18
yohagos <yosef.hagos@googlemail.com>
2024-01-23 13:06:04 +0100

    fix: apis and methods for management

--- src/main/java/com/titan/user/UserService.java
+++ src/main/java/com/titan/user/UserService.java
@@ -1,5 +1,7 @@
 package com.titan.user;
 
+import com.titan.user.request.UserAddRequest;
+import com.titan.user.request.UserEditRequest;
 import com.titan.user.request.UserPinUpdateRequest;
 import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
@@ -17,14 +19,13 @@ public class UserService {
 
     private final static Logger log = LoggerFactory.getLogger(UserService.class);
 
-
     public List<UserEntity> getUserList() {
         return userRepository.findAll();
     }
 
     public UserEntity updateUserPin(UserPinUpdateRequest request) {
         var user = userRepository.findById(request.getUserId()).orElseThrow();
-        if (request.getUserId() == null || (user.getPin() != null && request.getPin() == user.getPin()))
+        if (request.getUserId() == null || (user.getPin() != null && request.getPin().equals(user.getPin())))
             throw new IllegalArgumentException("Pin of User " + user.getUsername() + " cannot be changes");
         if (String.valueOf(request.getPin()).length() != 4)
             throw new IllegalArgumentException("Length of Pin has to be 4 digits");
@@ -40,5 +41,59 @@ public class UserService {
 
     public Optional<UserEntity> findUserById(Long id) {
         return userRepository.findById(id);
+    }
+
+    public UserEntity deleteUserById(Long id) {
+        var user = userRepository.findById(id).orElseThrow();
+        userRepository.deleteById(id);
+        return user;
+    }
+
+    public UserEntity addUser(UserAddRequest request) {
+        var user = userRepository.findUserByEmail(request.getEmail());
+        if (user.isPresent())
+            throw new IllegalArgumentException("email already in use");
+
+        user = userRepository.findUserByPin(request.getPin());
+        if (user.isPresent())
+            throw new IllegalArgumentException("pin already in use");
+
+        UserEntity newUser = new UserEntity();
+        newUser.setFirstname(request.getFirstname());
+        newUser.setLastname(request.getLastname());
+        newUser.setEmail(request.getEmail());
+        newUser.setPin(request.getPin());
+        var role = UserRole.valueOf(request.getRole().toUpperCase());
+        newUser.setRole(role);
+        return userRepository.save(newUser);
+    }
+
+    public UserEntity editUser(UserEditRequest request) {
+        var user = userRepository.findById(request.getId()).orElseThrow();
+
+        var role = UserRole.valueOf(request.getRole().toUpperCase());
+
+        Optional.ofNullable(request.getFirstname())
+                .filter(
+                        firstname -> !firstname.isEmpty() || !firstname.contentEquals(user.getFirstname())
+                ).ifPresent(user::setFirstname);
+        Optional.ofNullable(request.getLastname())
+                .filter(
+                        name -> !name.isEmpty() || !name.contentEquals(user.getLastname())
+                ).ifPresent(user::setLastname);
+        Optional.ofNullable(request.getEmail())
+                .filter(
+                        email -> !email.isEmpty() || !email.contentEquals(user.getEmail())
+                ).ifPresent(user::setEmail);
+        Optional.ofNullable(request.getPin())
+                .filter(
+                        pin -> pin != user.getPin()
+                ).ifPresent(user::setPin);
+        Optional.ofNullable(role)
+                .filter(
+                        r -> !r.equals(r)
+                ).ifPresent(user::setRole);
+        userRepository.save(user);
+        return user;
     }
 }
