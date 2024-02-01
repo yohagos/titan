68925a4b9cf19260acf6b9b4b7749a173dffeba0
yohagos <yosef.hagos@googlemail.com>
2024-02-01 14:05:27 +0100

    feat:: created ProductStock classes

---
+++ src/main/java/com/titan/product/stock/ProductsStockEntity.java
@@ -0,0 +1,37 @@
+package com.titan.product.stock;
+
+import com.titan.product.category.CategoryUnit;
+import com.titan.storage.StorageEntity;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class ProductsStockEntity implements Serializable {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @Enumerated(EnumType.STRING)
+    private CategoryUnit unit;
+    private Double measurement;
+    private StorageEntity good;
+
+
+    @Override
+    public String toString() {
+        return String.format(
+                "ProductStock=[id=%d, unit=%s, measurement=%s, good=%s]",
+                getId(), getUnit(), getMeasurement(), getGood()
+        );
+    }
+}
