111a165584a95f12b2ca265877807c4d3deb2b02
yohagos <yosef.hagos@googlemail.com>
2024-02-07 14:06:53 +0100

    fix: update repository

--- src/main/java/com/titan/storage/StorageRepository.java
+++ src/main/java/com/titan/storage/StorageRepository.java
@@ -2,5 +2,9 @@ package com.titan.storage;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface StorageRepository extends JpaRepository<StorageEntity, Long> {
+
+    Optional<StorageEntity> findByName(String name);
 }
