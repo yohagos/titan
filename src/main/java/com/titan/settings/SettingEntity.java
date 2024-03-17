7b64f910804f3e1a688496487345d6a1907384de
yohagos <yosef.hagos@googlemail.com>
2024-03-17 17:52:28 +0100

    feat: updated SettingController + SettingController + SettingEntity

--- src/main/java/com/titan/settings/SettingEntity.java
+++ src/main/java/com/titan/settings/SettingEntity.java
@@ -1,16 +1,15 @@
 package com.titan.settings;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
+
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.io.Serializable;
-import java.util.Map;
+import java.util.List;
 
 @Data
 @Builder
@@ -24,6 +23,23 @@ public class SettingEntity implements Serializable {
     @GeneratedValue
     private Long id;
 
+    private String companyName;
+    private String streetName;
+    private String streetNumber;
+    private String postalCode;
+    private String cityName;
 
-    /*private Map<String, String> settings;*/
+    private Boolean customColorTheme;
+    private String primaryColor;
+    private String accentColor;
+    private String warnColor;
+
+    private Integer timerLockScreen;
+
+    @Override
+    public String toString() {
+        return String.format("SettingEntity=[id=%d, companyName=%s, streetName=%s, streetNumber=%s, postalCode=%s, cityName=%s, customColorTheme=%s, primaryColor=%s, accentColor=%s, warnColor=%s, timerLockScreen=%d]",
+                getId(), getCompanyName(), getStreetName(), getStreetNumber(), getPostalCode(), getCityName(), getCustomColorTheme(), getPrimaryColor(), getAccentColor(), getWarnColor(), getTimerLockScreen());
+    }
+
 }
--- src/main/java/com/titan/settings/SettingService.java
+++ src/main/java/com/titan/settings/SettingService.java
@@ -1,11 +1,96 @@
 package com.titan.settings;
 
+import com.titan.settings.requests.EditSettingsRequest;
 import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
 public class SettingService {
 
     private final SettingRepository settingRepository;
+    private static final Logger log = LoggerFactory.getLogger(SettingService.class);
+
+    public List<SettingEntity> getSettings() {
+        return settingRepository.findAll();
+    }
+
+
+
+    public SettingEntity editSettings(EditSettingsRequest request) {
+        var findSetting = settingRepository.findById(1L);
+        var setting = findSetting.isPresent() ? editSetting(findSetting.get(), request) : addSettings(request);
+        return setting;
+    }
+
+    private SettingEntity addSettings(EditSettingsRequest request) {
+        var createdSettings = settingRepository.save(
+                new SettingEntity(
+                        1L,
+                        request.getCompanyName(),
+                        request.getStreetName(),
+                        request.getStreetNumber(),
+                        request.getPostalCode(),
+                        request.getCityName(),
+                        request.getCustomColorTheme(),
+                        request.getPrimaryColor(),
+                        request.getAccentColor(),
+                        request.getWarnColor(),
+                        request.getTimerLockScreen()
+                )
+        );
+        return createdSettings;
+    }
+
+    private SettingEntity editSetting(SettingEntity setting,EditSettingsRequest request) {
+        Optional.ofNullable(request.getCompanyName())
+                .filter(
+                        name -> !name.isEmpty()  || !name.contentEquals(setting.getCompanyName())
+                ).ifPresent(setting::setCompanyName);
+        Optional.ofNullable(request.getCityName())
+                .filter(
+                        name -> !name.isEmpty() || !name.contentEquals(setting.getCityName())
+                ).ifPresent(setting::setCityName);
+        Optional.ofNullable(request.getStreetName())
+                .filter(
+                        name -> !name.isEmpty() || !name.contentEquals(setting.getStreetName())
+                ).ifPresent(setting::setStreetName);
+        Optional.ofNullable(request.getPostalCode())
+                .filter(
+                        code -> !code.isEmpty() || !code.contentEquals(setting.getPostalCode())
+                ).ifPresent(setting::setPostalCode);
+        Optional.ofNullable(request.getStreetNumber())
+                .filter(
+                        number -> !number.isEmpty() || !number.contentEquals(setting.getStreetNumber())
+                ).ifPresent(setting::setStreetNumber);
+
+        Optional.of(request.getCustomColorTheme())
+                .filter(
+                        theme -> !theme.equals(setting.getCustomColorTheme())
+                ).ifPresent(setting::setCustomColorTheme);
+        Optional.of(request.getPrimaryColor())
+                .filter(
+                        prim -> !prim.isEmpty() || !prim.contentEquals(setting.getPrimaryColor())
+                ).ifPresent(setting::setPrimaryColor);
+        Optional.ofNullable(request.getAccentColor())
+                .filter(
+                        acc -> !acc.isEmpty() || !acc.contentEquals(setting.getAccentColor())
+                ).ifPresent(setting::setAccentColor);
+        Optional.ofNullable(request.getWarnColor())
+                .filter(
+                        warn -> !warn.isEmpty() || !warn.contentEquals(setting.getWarnColor())
+                ).ifPresent(setting::setWarnColor);
+        Optional.ofNullable(request.getTimerLockScreen())
+                .filter(
+                        timer -> timer > 0 || !timer.equals(setting.getTimerLockScreen())
+                ).ifPresent(setting::setTimerLockScreen);
+
+        settingRepository.save(setting);
+        return setting;
+    }
 }
